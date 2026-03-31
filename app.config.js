58fc051c7659264dc915736a8ae745091276369f
aashish47 <aashish27nov@gmail.com>
2026-03-31 16:13:25 +0530

    migrated to pnpm | multiple app variants

---
+++ app.config.js
@@ -0,0 +1,39 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getUniqueIdentifier = () => {
+	if (IS_DEV) {
+		return "com.aashish47.workout.dev";
+	}
+
+	if (IS_PREVIEW) {
+		return "com.aashish47.workout.preview";
+	}
+
+	return "com.aashish47.workout";
+};
+
+const getAppName = () => {
+	if (IS_DEV) {
+		return "Workout47 (Dev)";
+	}
+
+	if (IS_PREVIEW) {
+		return "Workout47 (Preview)";
+	}
+
+	return "Workout47";
+};
+
+export default ({ config }) => ({
+	...config,
+	name: getAppName(),
+	ios: {
+		...config.ios,
+		bundleIdentifier: getUniqueIdentifier(),
+	},
+	android: {
+		...config.android,
+		package: getUniqueIdentifier(),
+	},
+});
