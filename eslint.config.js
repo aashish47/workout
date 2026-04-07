1da6b8516a77a5cf8477fd3192471acf889d8c8c
aashish47 <aashish27nov@gmail.com>
2026-04-07 16:22:33 +0530

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-// https://docs.expo.dev/guides/using-eslint/
+// eslint.config.js
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const reactCompiler = require("eslint-plugin-react-compiler");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
 	expoConfig,
 	reactCompiler.configs.recommended,
+	eslintPluginPrettierRecommended,
 	{
 		ignores: ["dist/*"],
 	},
